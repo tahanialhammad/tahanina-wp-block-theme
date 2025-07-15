b5a469ae0f7ad82f9456e033f1663e2938c356ab
Tahani Alhammad <tahani_alhammad@yahoo.com>
2025-07-15 17:38:26 +0200

    update

--- patterns/header-social.php
+++ patterns/header-social.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * Title: Header Social links
+ * Slug: tahaninablocktheme/header-social
+ * Categories: header
+ * Block Types: core/template-part/header
+ * Description: header with centered site title and social links.
+ */
+
+?>
+
 <!-- wp:social-links {"iconColor":"background","iconColorValue":"#ffffff","iconBackgroundColor":"primary","iconBackgroundColorValue":"#007cba","size":"has-small-icon-size","className":"is-style-default"} -->
 <ul class="wp-block-social-links has-small-icon-size has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"https://tahanina.nl/","service":"facebook"} /-->
 
---
+++ patterns/hero-full-width-image.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: Hero, full width image
+ * Slug: tahaninablocktheme/hero-full-width-image
+ * Categories: banner
+ * Description: A hero with a full width image, heading, short paragraph and button.
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:cover {"dimRatio":50,"customOverlayColor":"#7a706b","isUserOverlayColor":false,"sizeSlug":"full","align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#7a706b"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="wp-block-heading has-text-align-center">Welcome to my website</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">information about my website and my services</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-primary-button"} -->
+<div class="wp-block-button is-style-primary-button"><a class="wp-block-button__link wp-element-button">Contact us</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
