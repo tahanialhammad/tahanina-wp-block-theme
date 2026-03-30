e971b0136a2cfa606f4d0ef48859137e7de81c4a
Tahani Alhammad <tahani_alhammad@yahoo.com>
2026-03-30 20:00:27 +0200

    update branch

---
+++ patterns/page-header.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Page Header with image cover
+ * Slug: tahanina/page-header
+ * Categories: banner
+ * Description:  Page Header with image cover and title
+ */
+?>
+
+<!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"overlayColor":"foreground","isUserOverlayColor":true,"minHeight":304,"minHeightUnit":"px","isDark":false,"sizeSlug":"full","style":{"border":{"radius":"20px"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="border-radius:20px;min-height:304px"><span aria-hidden="true" class="wp-block-cover__background has-foreground-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:post-title {"level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background"} /--></div></div>
+<!-- /wp:cover -->
